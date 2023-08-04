7906717e4ab809de9ab17f3575e42b889d244f32
Hannan Miah <hannanhridoy@gmail.com>
2023-08-05 01:38:12 +0600

    initial commit

---
+++ database/migrations/2023_08_04_190124_create_profiles_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->unique();
+            $table->foreignId('student_id')->constrained()->onDelete('cascade');
+            $table->string('fathers_name');
+            $table->string('mothers_name');
+            $table->string('district');
+            $table->string('upazila')->nullable();
+            $table->string('union')->nullable();
+            $table->string('village')->nullable();
+            $table->string('post_office')->nullable();
+            $table->string('post_code')->nullable();
+            $table->string('nid')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('signature')->nullable();
+            $table->string('nationality')->nullable();
+            $table->string('religion')->nullable();
+            $table->string('guardian_name')->nullable();
+            $table->string('guardian_phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
